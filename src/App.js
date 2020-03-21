1829a7748e04c67f5ffad3dc01f27397524c67e1
igallagi23 <32838995+igallagi23@users.noreply.github.com>
2020-03-21 12:22:54 +0200

    change to production serverIP

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Chat from "./Components/Chat";
 import NotFound from "./Components/NotFound";
 
 function App() {
-    App.serverIp='http://192.168.1.60:5000';
+    // App.serverIp='http://192.168.1.60:5000';
+    App.serverIp='18.222.99.147:5000';
     return (
         <Router>
             <Switch>
